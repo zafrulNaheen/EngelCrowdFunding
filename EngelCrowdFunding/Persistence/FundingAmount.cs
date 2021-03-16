b44caebf5bf86d50dcddc93f38a03cccbcdb268d
zafrul <naheen.bd@gmail.com>
2021-03-16 09:07:49 +0100

    Add funding post

---
+++ EngelCrowdFunding/Persistence/FundingAmount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EngelCrowdFunding.Persistence
+{
+    public class FundingAmount
+    { 
+        public Guid Id { get; set; }
+
+        public Guid FundingId { get; set; }
+
+        public double Amount { get; set; }
+
+    }
+}
