2314126b06336ade93e1e828c436393393a67b9a
zafrul <naheen.bd@gmail.com>
2021-03-16 07:16:04 +0100

    unnecessary files clean up

--- EngelCrowdFunding/ClientApp/src/app/home/home.component.ts
+++ EngelCrowdFunding/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+  selector: 'home',
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  public fundings: Funding[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.get<Funding[]>(baseUrl + 'fundings').subscribe(result => {
+      this.fundings = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Funding {
+  date: string;
+  projectName: string;
+  area: number;
+  description: string;
+  purchasePrice:number
 }
